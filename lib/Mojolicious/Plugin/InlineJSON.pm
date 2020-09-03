8bfb45b70cef0f949a16426a24b41469db2fddd7
Veesh Goldman <rabbiveesh@gmail.com>
2020-09-03 19:08:56 +0300

    up version to 1.0

--- lib/Mojolicious/Plugin/InlineJSON.pm
+++ lib/Mojolicious/Plugin/InlineJSON.pm
@@ -1,6 +1,6 @@
 package Mojolicious::Plugin::InlineJSON;
 
-our $VERSION = '0.009_001'; # 0.9.1
+our $VERSION = '1.000_000'; # 1.0.0
 $VERSION = eval $VERSION;
 
 use Mojo::Base 'Mojolicious::Plugin';
